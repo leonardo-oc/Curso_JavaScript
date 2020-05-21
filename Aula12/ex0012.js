80d9e77f73a1478a89cc5e3717d565f89e0d4f58
LeoII2 <48970930+LeoII2@users.noreply.github.com>
2020-05-20 21:05:41 -0400

    added Aula_13

---
+++ Aula12/ex0012.js
@@ -0,0 +1,12 @@
+var idade =68
+
+if (idade < 16){
+  console.log("Não Vota!")
+
+} else if( idade < 18 || idade > 65)
+    console.log("voto Opcional")
+  else {
+
+    console.log("voto Obrigatório")
+}
+
