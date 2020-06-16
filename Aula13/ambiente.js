12dd1394e6f3b60c8b4374aa5d8d1e0c61bd3509
LeoII2 <48970930+LeoII2@users.noreply.github.com>
2020-06-16 01:05:46 -0400

    added Aula_13

---
+++ Aula13/ambiente.js
@@ -0,0 +1,32 @@
+var c = 1
+do {
+
+  console.log(` Passo ${c}`)
+    c++
+
+}while(c <= 100)
+
+
+
+
+
+
+/*var c = 1
+
+while (c < 100) {
+
+    console.log(` Passo ${c}`)
+    c++
+}
+*/
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
